14d7dd51a898a0beaacde65a5d19137ffdbcc1fe
Ashwin Kumar <hmashwinkumar@gmail.com>
2023-12-15 22:27:43 +0530

    After creating Class for capturing the Date of Service and Lab Results

--- test.py
+++ test.py
@@ -5,27 +5,27 @@ from pdfminer.high_level import extract_pages, extract_text
 import re
 import os
 
-#text = extract_text('D:\\Newfolder\\Per\\Healthview\\Reports\\49_61539409_0_1.pdf')
-#text1 = extract_text('D:\\Newfolder\\Per\\Healthview\\Reports\\MED120473722_49_61451353 (1).pdf')
+class LabDataObject(object):
+    def __init__(self):
+        self.dateoftest = ''
+        self.labResult = ''
 
-dir = 'D:\\Newfolder\\Per\\Healthview\\Reports'
+dir = 'D:\\Newfolder\\Per\\Healthview\\Reports\\Sugar'
 files = os.listdir(dir)
 index = 0
 
 c1 = 0
-results = {'HBA1C': []}
+results = []
+labresults = []
 
 for file in files:
     text = extract_text(dir + '\\' + file )
-    reportType = file[0:10]
+    reportType = "Only HBA1C"
+        #file[0:10]
     #print(reportType)
     reportType1 = file[0:14]
-    #print(reportType1)
-    #print(reportType)
-    #print(file)
+    l = LabDataObject()
     if reportType == "Only HBA1C":
-        #results = {'HBA1C':[]}
-        #print(reportType)
         c1 = 0
         date = ''
         for t in text.split('\n'):
@@ -33,57 +33,37 @@ for file in files:
             if c1 == 52:
                 date = t.strip()
                 c1 += 1
+                l.dateoftest = date
+                results.append(date)
                 #print(t.strip())
                 #print( ' t.strip() + ' + t.strip())
             elif c1 == 77:
-                #results['HBA1C'] += t.strip()
-                results['HBA1C'].append(date)
-                #results['HBA1C'].append(t.strip())
-                results['HBA1C'].append(t.strip())
-                #print(t.strip())
-                #print(results)
+                l.labResult = t.strip()
                 c1 += 1
-               # print(c1)
             else:
                 c1 += 1
-    elif reportType1 == "Complete Blood":
-        #print(reportType1)
-        #results = {'HBA1C': []}
+        labresults.append(l)
+    elif reportType == "Complete Blood":
         d1 = 0
         date1 = ''
         for t in text.split('\n'):
-            #print(t + ' - ' + str(d1) )
-            #print(d1)
             if d1 == 52:
-                #print(d1)
-                #print(t.strip())
+                # print(d1)
+                # print(t.strip())
                 date1 = t.strip()
                 d1 += 1
+                l.dateoftest = date1
+                results.append(date1)
             elif d1 == 934:
-                #results['HBA1C'] += t.strip()
-                results['HBA1C'].append(date1)
-                #results['HBA1C'].append(t.strip())
-                results['HBA1C'].append(t.strip())
-                #print(results)
-                #print(t.strip())
+                l.labResult = t.strip()
                 d1 += 1
             else:
-                #print(d1)
                 d1 += 1
-            #d1 += 1
-print(results)
+        labresults.append(l)
 
-            #print(t)
-            #print(c1)
-            #c1 += 1
+for e in labresults:
+    print(e.dateoftest)
+    print(e.labResult)
 
-    #c1 =0
-
-
-    #HbA1C
-#pattern = re.compile(r"[HbA1C]")
-#matches = pattern.findall(text)
-
-        #print(ch.strip())
-
-
+for key in results:
+    print(key)
