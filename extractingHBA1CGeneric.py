9eaf42150b635f97739d767715c28dfa415d67c6
Ashwin Kumar <hmashwinkumar@gmail.com>
2023-12-16 05:38:02 +0530

    After enhancing the scipt to extract the Collection On date along with the HBA1C value

--- extractingHBA1CGeneric.py
+++ extractingHBA1CGeneric.py
@@ -2,41 +2,47 @@ import os
 import re
 from pdfminer.high_level import extract_text
 
-
-# Function to extract HbA1c value from PDF
-def extract_hba1c_value(pdf_path):
+def extract_hba1c_and_collection_on(pdf_path):
     # Extract text from the PDF
     pdf_text = extract_text(pdf_path)
 
-    # Use a regular expression to search for the HbA1c value
-    hba1c_match = re.search(r'HbA1C\s*\(Whole Blood/HPLC\)\s*([\d.]+)', pdf_text, re.IGNORECASE)
+    # Use a regular expression to search for the HbA1C value
+    hba1c_match = re.search(r'HbA1C[^0-9]*(\d+\.\d+)', pdf_text, re.IGNORECASE)
+
+    hba1c_value = None
 
     if hba1c_match:
         hba1c_value = hba1c_match.group(1)
-        # Convert the value to float if it contains valid numeric characters
-        try:
-            return float(hba1c_value)
-        except ValueError:
-            pass
 
-    return None
+    # Use regex to search for the Collection On date
+    collection_on_match = re.search(r'Collection\s*On[^0-9]*([\d\/]+\s*[\d:]+\s*[APMapm]+)', pdf_text, re.IGNORECASE)
 
+    collection_on_date = None
 
-# Directory containing PDF files
-pdf_directory = 'D:\\Newfolder\\Per\\Healthview\\Reports\\Sugar\\Ashwin'
+    if collection_on_match:
+        # Fetch the value from one line above
+        collection_on_line = collection_on_match.group(0)
+        collection_on_date_match = re.search(r'(\d+/\d+/\d+\s*[\d:]+\s*[APMapm]+)', collection_on_line)
+        collection_on_date = collection_on_date_match.group(1).strip() if collection_on_date_match else None
 
-# Get a list of PDF files in the directory
-pdf_files = [file for file in os.listdir(pdf_directory) if file.endswith('.pdf')]
+    return hba1c_value, collection_on_date
 
-# Iterate through each PDF file
-for pdf_file in pdf_files:
-    pdf_path = os.path.join(pdf_directory, pdf_file)
+def process_files_in_directory(directory_path):
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".pdf"):
+            pdf_path = os.path.join(directory_path, filename)
+            hba1c_value, collection_on_date = extract_hba1c_and_collection_on(pdf_path)
 
-    # Extract HbA1c value from the PDF
-    hba1c_value = extract_hba1c_value(pdf_path)
+            if hba1c_value:
+                print(f"The HbA1C value in {filename} is: {hba1c_value}")
+            else:
+                print(f"No HbA1C value found in {filename}")
 
-    # Print the result
-    if hba1c_value is not None:
-        print(f"The HbA1c value in {pdf_file} is: {hba1c_value}")
-    else:
-        print(f"No HbA1c value found in {pdf_file}")
+            if collection_on_date:
+                print(f"The Collection On date in {filename} is: {collection_on_date}")
+            else:
+                print(f"No Collection On date found in {filename}")
+
+# Specify the directory containing your PDF files
+pdf_directory = "D:\\Newfolder\\Per\\Healthview\\Reports\\Sugar\\Ashwin"
+process_files_in_directory(pdf_directory)
